1e454753e889533fcccf14a9d11f56e7875ccbe4
Andrei Razvan Oproiu <andrei.r.oproiu@gmail.com>
2020-07-11 16:22:04 +0100

    created version 4

---
+++ version_4/src/components/Table.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-table striped hover :items='items' :fields="fields">
+  </b-table>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [],
+      fields: [
+        {
+          key: 'age',
+          label: 'Idade',
+          sortable: true
+        },
+        {
+          key: 'first_name',
+          label: 'Primeiro Nome',
+          sortable: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ version_4/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Table />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from '../components/Table.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Table
+  }
+}
+
+</script>
