20a4de322900e78c667a59a06666b7c186d6eac9
Andrei Oproiu <andrei.r.oproiu@gmail.com>
2020-07-18 16:30:17 +0100

    weather widget completed

--- projeto_final/src/components/WeatherInfo.vue
+++ projeto_final/src/components/WeatherInfo.vue
@@ -3,19 +3,26 @@
   <div>
     <!-- <p>{{ cityId }}</p> -->
     <!-- <h1>{{translatedName(cityId)}}</h1> -->
-    <b-card
-     
-     
-      img-left
-      tag="article"
-      style="max-width: 30rem;"
-      class="mb-2"
-    >
+    <b-card img-left tag="article" style="max-width: 30rem;" class="mb-2">
       <b-card-text>
-        <small class="text-muted">Última actualização: {{cityPred.dataUpdate.split('T')[1]}} de {{cityPred.dataUpdate.split('T')[0]}}</small>
-        <h1>{{translatedName(cityId)}}</h1>
-        <h4>Max: {{cityPred.data[0].tMax}} ºC</h4>
-        <h4>Min: {{cityPred.data[0].tMa} ºC</h4>
+        <div class="row">
+          <div class="col-sm-6">
+            <img src="../assets/weather-icons-master/svg/wi-windy.svg" alt="sdasd">
+          </div>
+          <div class="col-sm-8">
+            
+            <h1>{{translatedName(cityId)}}</h1>
+            <h4>{{translatedWeather(cityPred.data[0].classWindSpeed)}}</h4>
+            <h4>Max: {{cityPred.data[0].tMax}} ºC</h4>
+            <h4>Min: {{cityPred.data[0].tMin}} ºC</h4>
+            <h4>Precipitação: {{cityPred.data[0].precipitaProb}} %</h4>
+            <h4>Vento: {{translatedWind(cityPred.data[0].idWeatherType)}}</h4>
+            <small
+              class="text-muted"
+            >Última actualização: {{cityPred.dataUpdate.split('T')[1]}} de {{cityPred.dataUpdate.split('T')[0]}}</small>
+          </div>
+          
+        </div>
       </b-card-text>
     </b-card>
   </div>
@@ -25,14 +32,14 @@
 import { mapState } from "vuex";
 
 export default {
-  data(){
+  data() {
     return {
-      weather: 'sdsd',
-    }
+      weather: "sdsd"
+    };
   },
   props: ["cityId"],
   mounted() {
-    this.$store.dispatch("getCityPred", this.cityId)
+    this.$store.dispatch("getCityPred", this.cityId);
   },
   computed: {
     ...mapState(["cityPred", "distIsles", "weatherType", "windType"])
@@ -47,6 +54,24 @@ export default {
       }
 
       return cityId + "- Unknown";
+    },
+    translatedWind(windId){
+      for (let i = 0; i < this.windType.data.length; i++) {
+        if (windId == this.windType.data[i].classWindSpeed) {
+          return this.windType.data[i].descClassWindSpeedDailyPT;
+        }
+      }
+
+      return windId + "- Unknown";
+    },
+    translatedWeather(weatherId){
+      for (let i = 0; i < this.weatherType.data.length; i++) {
+        if (weatherId == this.weatherType.data[i].idWeatherType) {
+          return this.weatherType.data[i].descIdWeatherTypePT;
+        }
+      }
+
+      return weatherId + "- Unknown";
     }
   }
 };
